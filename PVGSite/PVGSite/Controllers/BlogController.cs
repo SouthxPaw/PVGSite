3f0b99cdfbe56745041b18cd4eccd86e5ea43e17
SouthxPaw <jordanaletraris@gmail.com>
2020-03-03 21:52:14 -0500

    Added all of the pages for the site

---
+++ PVGSite/PVGSite/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PVGSite.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
---
+++ PVGSite/PVGSite/Controllers/CalendarController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PVGSite.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+    }
+}
