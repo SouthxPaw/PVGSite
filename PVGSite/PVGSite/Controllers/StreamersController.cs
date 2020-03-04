2a09ce73f6b5155ea373934173acc0d6ab4c3185
SouthxPaw <jordanaletraris@gmail.com>
2020-03-03 21:10:29 -0500

    initial push

---
+++ PVGSite/PVGSite/Controllers/StreamersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PVGSite.Controllers
+{
+    public class StreamersController : Controller
+    {
+        public ActionResult awaken()
+        {
+            return View();
+        }
+
+        public ActionResult woxdy()
+        {
+            return View();
+        }
+
+        public ActionResult wingspan()
+        {
+            return View();
+        }
+
+        public ActionResult RV()
+        {
+            return View();
+        }
+    }
+}
